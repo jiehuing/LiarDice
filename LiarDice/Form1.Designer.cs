0213efe3d4051a265129a64e48f33652066a5579
jiehuing <58838781+jiehuing@users.noreply.github.com>
2021-10-17 12:21:20 +0800

    Add files via upload

---
+++ LiarDice/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace LiarDice
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(161, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(391, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Roll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(307, 70);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(134, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(474, 70);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(201, 192);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(413, 192);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 450);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ LiarDice/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LiarDice.Properties;
+
+namespace LiarDice
+{
+    public partial class Form1 : Form
+    {
+        int Dice1, Dice2, Dice3, Dice4, Dice5;
+        int time=0;
+        Random rng;
+        System.Media.SoundPlayer rollingDice = new System.Media.SoundPlayer(@"C:\Users\ngjh9\source\repos\LiarDice\Resources\Rolling Dice.wav");
+        System.Media.SoundPlayer rollingDiceStopping = new System.Media.SoundPlayer(@"C:\Users\ngjh9\source\repos\LiarDice\Resources\Rolling Dice Stopping.wav");
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (time%11==0)
+            {
+                rollingDice.Play();
+            }
+
+            time += 1;
+            if (time == 30)
+            {
+                rollingDiceStopping.Play();
+                rollingDice.Stop();
+                timer1.Stop();
+                time = 0;
+            }
+            else
+            {
+                //Generate new dice numbers
+                rng = new Random();
+                if (time < 22)
+                {
+                    Dice1 = rng.Next(1, 6);
+                }
+                if (time<24)
+                {
+                    Dice2 = rng.Next(1, 6);
+                }
+                if(time<26)
+                {
+                    Dice3 = rng.Next(1, 6);
+                }
+                if (time<28)
+                {
+                    Dice4 = rng.Next(1, 6);
+                }
+                Dice5 = rng.Next(1, 6);
+
+
+                //Attach picture to dice face
+                attachPicture(Dice1, pictureBox1);
+                attachPicture(Dice2, pictureBox2);
+                attachPicture(Dice3, pictureBox3);
+                attachPicture(Dice4, pictureBox4);
+                attachPicture(Dice5, pictureBox5);
+            }
+        }
+
+        private void attachPicture(int dice, PictureBox pictureBox)
+        {
+            switch (dice)
+            {
+                case 1:
+                    pictureBox.Image = Resources.dice1;
+                    break;
+                case 2:
+                    pictureBox.Image = Resources.dice2;
+                    break;
+                case 3:
+                    pictureBox.Image = Resources.dice3;
+                    break;
+                case 4:
+                    pictureBox.Image = Resources.dice4;
+                    break;
+                case 5:
+                    pictureBox.Image = Resources.dice5;
+                    break;
+                case 6:
+                    pictureBox.Image = Resources.dice6;
+                    break;
+
+            }
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
